7ff098dfb1c524c47c15c3716c5c177cb0a45352
acmarchiori <ac_marchiori@yahoo.com.br>
2022-10-21 13:34:00 -0300

    Adicionado 5 primeiros exercicios

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/maiorNumero.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 29996;
+const c =55;
+
+if (a > b && a > c) {
+  console.log('O maior número é a = ' + (a));
+}
+else if (b > a && b > c) {
+  console.log('O maior número é b = ' + (b));
+}
+else {
+  console.log('O maior número é c = ' + (c));
+}
+
