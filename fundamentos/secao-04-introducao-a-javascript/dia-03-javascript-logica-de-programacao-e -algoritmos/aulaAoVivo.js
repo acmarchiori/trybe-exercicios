176de3458442aaab7932aeb93c308cdc44cf9723
acmarchiori <ac_marchiori@yahoo.com.br>
2022-10-26 21:31:13 -0300

    Finalizando exercicios dia 03

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e -algoritmos/aulaAoVivo.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e -algoritmos/aulaAoVivo.js
@@ -88,7 +88,7 @@
 // Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade. Imprima no console seguindo o exemplo: “A pessoa é maior de idade”.
 // Bônus: Crie a condição utilizando operador ternário.
 
-let pessoa = 'Antonio';
-let idade = 17;
+let pessoa = 'Daniele';
+let idade = 40;
 
 idade >= 18 ? console.log(pessoa, 'é maior de idade') : console.log(pessoa, 'é menor de idade');
